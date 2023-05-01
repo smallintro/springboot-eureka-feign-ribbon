ee557840d0fd4f53b67b1160390384905f4ef336
Sushil Prasad <cse.sushil@gmail.com>
2023-05-01 23:03:49 +0530

    WebClientConfiguration

--- seller-ribbon-client/src/main/java/com/smallintro/springboot/seller/SellerApplication.java
+++ seller-ribbon-client/src/main/java/com/smallintro/springboot/seller/SellerApplication.java
@@ -2,11 +2,7 @@ package com.smallintro.springboot.seller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
@@ -15,11 +11,5 @@ public class SellerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SellerApplication.class, args);
-    }
-
-    @Bean
-    @LoadBalanced
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
     }
 }
